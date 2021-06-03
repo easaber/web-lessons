819f918f1bb4c0b2c419b345f4043cf707411d67
Emmanuel Asaber <emmanuel.asaber@amalitech.org>
2021-06-03 12:19:46 +0000

    Find

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-let numbers = [1,2,3,1,4,5,1]
 
-const counter = (number, array) => {
-    let count = 0;
-    for(let num of array) {
-        if(num === number) {
-            count++;
-        }
+let trainers = [
+    {
+        name: 'Thomas'
+    },
+    {
+        name: 'Salami'
+    },
+    {
+        name: 'Amo'
+    },
+    {
+        name: 'King'
     }
+]
 
-    return count;
-}
 
-console.log(counter(3,[1,2,3,1,4,5,1,2]))
+
+let salami = trainers.find((trainer,index,trainers) => {
+    return trainer.name === 'Salami'
+});
+
+console.log(salami)
